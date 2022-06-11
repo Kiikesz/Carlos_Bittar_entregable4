b9cf256a9a3358b59035b3708d021c938277119d
Kiikesz <106416484+Kiikesz@users.noreply.github.com>
2022-06-11 12:54:28 -0500

    Carlos_Bittar
    entregable 4 Carlos Bittar

---
+++ productos.js
@@ -0,0 +1,73 @@
+class producto {
+    constructor(id, nombre, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+}
+
+class itemCarrito {
+    constructor(id, nombre, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+}
+
+
+
+
+
+function cargaproducto() {
+    productos.push(new producto(1,"Bandas Adhesivas Kids 20uds Zuviss", 1.00))
+    productos.push(new producto(2,"Bacitracina  500 Ui/g 15g SGG", 18.19))
+    productos.push(new producto(3,"Azul de Metileno Fsi 30Cc.", 5.56))
+    productos.push(new producto(4,"Azitromicina Kern Comp. 500 Mg X 3", 5.56))
+    productos.push(new producto(5,"Azitromicina Buka Tab 500 Mg X 5", 3.89))
+    productos.push(new producto(6,"Asma II Gotas 20ml", 4.44))
+    productos.push(new producto(7,"Arnica D6 Globulis 8g", 2.77))
+    productos.push(new producto(8,"Arnica D30 Globulis 8g", 4.16))
+    productos.push(new producto(9,"Arnica 7Ch Globulis 8g", 4.44))
+    productos.push(new producto(10,"Aranda  2,5/50 X30Capsulas", 1.00))
+    productos.push(new producto(11,"Apiret Acetaminofén Pediátrico 60ml", 2.79))
+    productos.push(new producto(12,"Antinelle Dia Kern Comp 0.02Mg/3Mg", 18.48))
+    productos.push(new producto(13,"Antifom 40Mg 10Comp", 1.00))
+    productos.push(new producto(14,"Anthelios Xl Compact. Spf50 9G Dor 02 La Roche - Posay", 1.00))
+    productos.push(new producto(15,"Anamu Caps 400Mg X60 Herbaplant", 4.66))
+    productos.push(new producto(16,"Anafilaxia Gotas 20Cc Prod. Homeopático", 4.51))
+    productos.push(new producto(17,"Alcohol P/Quemar Fsi 250Cc", 4.63))
+    productos.push(new producto(18,"Alcohol Extrafino Fsi 250 Cc", 7.09))
+    productos.push(new producto(19,"Albendazol 200mg x 6 tbl La Sante", 1.22))
+    productos.push(new producto(20,"Ajo Cap 570Mgx60 Natural Premium", 8.86))
+    productos.push(new producto(21,"Agua Destilada Laboratorios FSI 960 Cc", 1.30))
+    productos.push(new producto(22,"Addult Protector Incontinencia Leve 6", 4.96))
+    productos.push(new producto(23,"Addult Protector Incontinencia Leve 24", 1.00))
+    productos.push(new producto(24,"Addult Protector Incontinencia Leve 12", 1.00))
+    productos.push(new producto(25,"Actimove Cervical Collar Kids Inf Xxs", 2.62))
+    productos.push(new producto(26,"Acido Bórico 20Gr Laboratorios FSI", 3.48))
+    productos.push(new producto(27,"Aceite Mineral Extrafino Farmagenik 30Cc", 5.76))
+    productos.push(new producto(28,"Aceite De Ricino Fsi 30 Cc", 1.00))
+    productos.push(new producto(29,"Abretia Caps.25Mg X20", 4.25))
+    productos.push(new producto(30,"Domperidona Calox 10 mg 15 Tableta", 1.00))
+    productos.push(new producto(31,"Lidocaina Ale Sol.Iny 2% Amp.30Ml", 6.45))
+    productos.push(new producto(32,"Spray Blue Cap 50 cc", 4.36))
+    productos.push(new producto(33,"Quetiapina ObMedical 100 mg 30 Tabletas", 7.52))
+    productos.push(new producto(34,"Enalapril ObMedical 10 mg 30 tabletas", 15.80))
+    productos.push(new producto(35,"Carvedilol ObMedical 12,5 mg 30 tabletas", 20.44))
+    productos.push(new producto(36,"Termometro Rectal Clinico Mavica 12 uds", 3.28))
+    productos.push(new producto(37,"Ácido Fólico Genven 5 mg 30 Comprimidos", 7.12))
+    productos.push(new producto(38,"Etoricoxib Calox 60 mg 10 Tabletas", 6.05))
+    productos.push(new producto(39,"Unguento Adel 50 g", 1.94))
+    productos.push(new producto(40,"Roll On Menthol Ice Gel 60 g", 1.00))
+    productos.push(new producto(41,"Enjuague Bucal Brucen cool mint 250 ml", 11.12))
+    productos.push(new producto(42,"Alcohol Antiséptico concrentrado al 70% Sanol 120 ml", 4.76))
+    productos.push(new producto(43,"Crema para piernas cansadas Vibra Natural 200 g", 8.09))
+    productos.push(new producto(44,"Pasta Al Agua Rcm 150Gr", 1.00))
+    productos.push(new producto(45,"Arnica Fc Gel Roll-On Extract.Mango 90", 1.00))
+    productos.push(new producto(46,"Gel antibacterial Brucen 300 cc", 7.49))
+    productos.push(new producto(47,"Magnesio Aminoquelado Pharmatech 30 Cápsulas Blandas", 1.00))
+    productos.push(new producto(48,"Cardo Mariano Milenium's 300 mg 100 cap", 1.00))
+    productos.push(new producto(49,"Levinzol 1000 mg 30 Cápsulas", 4.44))
+    productos.push(new producto(50,"Aceite 3-6-9 de Linaza Omega 60 Cápsulas blandas", 2.82))
+    
+}
